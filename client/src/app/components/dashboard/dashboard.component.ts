622b499dd567c93c8f0229dd73579ffc03ec42c8
Jesús GS <jesalceg@gmail.com>
2021-11-23 12:13:22 -0500

    Working on dashboard

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -3,44 +3,53 @@ import { UsersComponent } from '../functions/users/users.component';
 import { HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  sidebarToggle: boolean = true;
-  readerPanel: boolean = false;
-  homePanel: boolean = true;
+    sidebarToggle: boolean = true;
+    readerPanel: boolean = false;
+    homePanel: boolean = true;
+    active: boolean = false;
 
 
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    window.innerWidth > 768 ? this.sidebarToggle = true : this.sidebarToggle = false;
-  }
-
-  constructor() {
-   }
-
-  ngOnInit(): void {
-    this.homePanel = true;
-  }
-
-
-
-
-  showReaderPanel() {
-    this.readerPanel = true;
-    this.homePanel = false;
-  }
-
-  toggleSidebar() {
-    if (this.sidebarToggle) {
-      this.sidebarToggle = false;
-    }else {
-      this.sidebarToggle = true;
-      console.log('toggled');
+    @HostListener('window:resize', ['$event'])
+    onResize() {
+        window.innerWidth > 768 ? this.sidebarToggle = true : this.sidebarToggle = false;
     }
-  }
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        this.homePanel = true;
+    }
+
+
+    setActive(){
+        if (!this.active){
+            this.active = true;
+            console.log(this.active);
+        }else {
+            this.active = false;
+            console.log(this.active);
+        }
+    }
+
+    showReaderPanel() {
+        this.readerPanel = true;
+        this.homePanel = false;
+    }
+
+    toggleSidebar() {
+        if (this.sidebarToggle) {
+            this.sidebarToggle = false;
+        } else {
+            this.sidebarToggle = true;
+            console.log('toggled');
+        }
+    }
 
 }
